9e2a9b0c37e5fd23793aad129beb3a5bbfb48d1a
Thiago Kaique <66787043+Thiago099@users.noreply.github.com>
2024-05-04 20:07:21 -0300

    added bookmark function

---
+++ Bookmark.py
@@ -0,0 +1,11 @@
+#TODO write a description for this script
+#@author 
+#@category _NEW_
+#@keybinding 
+#@menupath 
+#@toolbar 
+
+
+#TODO Add User Code Here
+
+createBookmark(currentAddress, "Addresses", "Description")
